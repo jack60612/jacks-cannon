804870de6b8832268153051d9e6e67394621495a
Jack Nelson <jack@jacknelson.xyz>
2024-06-30 02:09:43 -0400

    make async + add webserver

--- cannon/cannon.py
+++ cannon/cannon.py
@@ -1,24 +1,68 @@
-import time
+import asyncio
 
 from cannon.buttons.fire_button import FireButton
 from cannon.buttons.safety_button import SafetyButton
+from cannon.network_server import NetworkServer
 from cannon.relay import Relay
+
+
+class Cannon:
+    """
+    Class for the cannon
+    """
+
+    fire_button: FireButton
+    safety_button: SafetyButton
+    main_relay: Relay
+    network_server: NetworkServer
+
+    def __init__(self) -> None:
+        """
+        Initialize the cannon
+        """
+        self.fire_button: FireButton = FireButton()
+        self.safety_button: SafetyButton = SafetyButton()
+        self.main_relay: Relay = Relay()
+        self.network_server: NetworkServer = NetworkServer(self.fire_cannon)
+
+    async def start(self) -> None:
+        """
+        Main function for the cannon
+        :return:
+        """
+        await self.network_server.start()  # Start the network server
+        # Core loop
+        while True:
+            if self.fire_button.is_pressed():
+                print("Fire button pressed")
+                await self.fire_cannon()
+            await asyncio.sleep_ms(100)
+
+    async def fire_cannon(self) -> None:
+        """
+        Fire the cannon
+        """
+        milliseconds: int = 100
+        if self.safety_button.is_pressed():
+            print("Safety released, firing")
+            self.main_relay.relay_on()
+            print("Solenoid on")
+            await asyncio.sleep_ms(milliseconds)
+            self.main_relay.relay_off()
+            print("Solenoid off")
+        else:
+            print("Safety not released, not firing")
 
 
 def main() -> None:
     """
-    Main function for the cannon
+    Starts event loop
     """
-    fire_button: FireButton = FireButton()
-    safety_button: SafetyButton = SafetyButton()
-    main_relay: Relay = Relay()
-    while True:
-        if safety_button.is_pressed():
-            if fire_button.is_pressed():
-                main_relay.relay_on()
-                time.sleep_ms(100)
-                main_relay.relay_off()
-        time.sleep_ms(100)
+    cannon: Cannon = Cannon()
+    try:
+        asyncio.run(cannon.start())
+    finally:
+        asyncio.new_event_loop()
 
 
 if __name__ == "__main__":
---
+++ cannon/network_server.py
@@ -0,0 +1,82 @@
+import asyncio
+import socket
+import time
+from typing import Awaitable, Callable
+
+import network
+
+from cannon.constants import Constants
+
+
+class NetworkServer:
+    """
+    Class for the network server & Wifi Access Point
+    """
+
+    html: str
+    wlan: network.WLAN
+    addr: tuple[str, int]
+    webserver_socket: socket.Socket
+    trigger_callback: Callable[[], Awaitable[None]]
+
+    def __init__(self, trigger_callback: Callable[[], Awaitable[None]]) -> None:
+        """
+        Initialize the network server
+        """
+        self.start_wifi()
+        self.trigger_callback: Callable[[], Awaitable[None]] = trigger_callback
+        self.html = """<!DOCTYPE html>
+<html>
+    <head> <title>Pico W</title> </head>
+    <body> <h1>Pico W</h1>
+        <p>%s</p>
+    </body>
+</html>
+"""
+
+    def start_wifi(self) -> None:
+        """
+        Start the WI-FI access point
+        """
+        self.wlan: network.WLAN = network.WLAN(network.AP_IF)
+        self.wlan.config(essid=Constants.ssid, password=Constants.password)
+        self.wlan.active(True)
+        # wait for connection to come up
+        while not self.wlan.active():
+            time.sleep(1)
+        print("AP Mode Is Active, You can Now Connect")
+        print("IP Address To Connect to:: " + self.wlan.ifconfig()[0])
+
+    async def serve_client(self, reader, writer):
+        print("Client connected")
+        request_line = await reader.readline()
+        print("Request:", request_line)
+        # We are not interested in HTTP request headers, skip them
+        while await reader.readline() != b"\r\n":
+            pass
+
+        request = str(request_line)
+        triggered = request.find("/trigger/activate")
+        print("triggered remotely = " + str(triggered))
+
+        stateis = ""
+        if triggered == 6:
+            print("triggered remotely")
+            await self.trigger_callback()
+            stateis = "triggered"
+
+        response = self.html % stateis
+        writer.write("HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\n")
+        writer.write(response)
+
+        await writer.drain()
+        await writer.wait_closed()
+        print("Client disconnected")
+
+    async def start(self) -> None:
+        """
+        Start the webserver
+        """
+        await asyncio.create_task(
+            asyncio.start_server(self.serve_client, "0.0.0.0", 80)
+        )
